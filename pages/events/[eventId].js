feb713d094b7321fc6b39f767251f9c96e318ecd
Mosiva <portret008@gmail.com>
2024-02-10 14:41:09 +0600

    finished [eventid]

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import React from "react";
 import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 export default function EventDetailPage() {
   const router = useRouter();
@@ -11,5 +14,18 @@ export default function EventDetailPage() {
     return <p>No event found!</p>;
   }
 
-  return <div>EventDetailPage</div>;
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
