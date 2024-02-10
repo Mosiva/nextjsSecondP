6bb30cbf4591fd658573a116f7ffe2dfab7cc612
Mosiva <portret008@gmail.com>
2024-02-10 15:58:42 +0600

    add logic to filter

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
+import { useRouter } from "next/router";
 
 export default function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
