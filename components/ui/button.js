6d531b8491bb87d0d4c1a41b00c5c7935bfaece2
Mosiva <portret008@gmail.com>
2024-02-10 15:38:39 +0600

    add filter component

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,10 +2,17 @@ import Link from "next/link";
 import React from "react";
 import classes from "./button.module.css";
 
-export default function Button({ children, link }) {
+export default function Button({ children, link, onClick }) {
+  if (link) {
+    return (
+      <Link href={link} className={classes.btn}>
+        {children}
+      </Link>
+    );
+  }
   return (
-    <Link href={link} className={classes.btn}>
+    <button className={classes.btn} onClick={onClick}>
       {children}
-    </Link>
+    </button>
   );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/event-search";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <>
+      <EventSearch />
       <EventList items={events} />
-    </div>
+    </>
   );
 }
