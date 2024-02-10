d5780971eb6aa2ce2a3718d618f0b865acda87b7
Mosiva <portret008@gmail.com>
2024-02-10 14:33:07 +0600

    add event-detail folder

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,9 @@
-import Link from "next/link";
 import React from "react";
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem({ title, image, date, location, id }) {
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -20,13 +22,20 @@ export default function EventItem({ title, image, date, location, id }) {
           <h2>{title}</h2>
         </div>
         <div className={classes.date}>
+          <DateIcon />
           <time>{humanReadableDate}</time>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,15 @@
+import { useRouter } from "next/router";
 import React from "react";
+import { getEventById } from "../../dummy-data";
 
 export default function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return <div>EventDetailPage</div>;
 }
